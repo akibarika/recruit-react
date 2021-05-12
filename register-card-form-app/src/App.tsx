58f6d170a208405372a762cd08cb73f7fbf0aa10
Akibarika <lxclxc89816@gmail.com>
2021-05-12 22:27:39 +1200

    Add @types/react-router-dom and implement into App

--- register-card-form-app/src/App.tsx
+++ register-card-form-app/src/App.tsx
@@ -1,36 +1,22 @@
 import React from 'react';
 import './App.css'
-import {makeStyles} from '@material-ui/core/styles';
-import {AppBar, Toolbar, IconButton, Typography} from '@material-ui/core'
-import MenuIcon from '@material-ui/icons/Menu';
+import {
+    BrowserRouter as Router,
+    Switch,
+    Route
+} from "react-router-dom";
 
-const useStyles = makeStyles((theme) => ({
-    root: {
-        flexGrow: 1,
-    },
-    menuButton: {
-        marginRight: theme.spacing(2),
-    },
-    title: {
-        flexGrow: 1,
-    },
-}));
+import RegisterCardForm from './components/CreditCardForm';
+import Menu from './components/Menu';
 
-function App() {
-    const classes = useStyles();
+const App = () => {
     return (
-        <>
-            <AppBar position="static">
-                <Toolbar>
-                    <IconButton edge="start" className={classes.menuButton} color="inherit" aria-label="menu">
-                        <MenuIcon/>
-                    </IconButton>
-                    <Typography variant="h6" className={classes.title}>
-                        Register card form
-                    </Typography>
-                </Toolbar>
-            </AppBar>
-        </>
+        <Router>
+            <Switch>
+                <Route path="/" component={RegisterCardForm} exact/>
+                <Route path="/menu" component={Menu} exact/>
+            </Switch>
+        </Router>
     );
 }
 
---
+++ register-card-form-app/src/components/Menu.tsx
@@ -0,0 +1,9 @@
+import React from 'react';
+
+const Menu = () => (
+    <>
+        This is Menu
+    </>
+)
+
+export default Menu;
