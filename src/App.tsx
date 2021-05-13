4b290afecd8e7d3e0ec1b42437fde48172aa319d
Akibarika <lxclxc89816@gmail.com>
2021-05-13 14:01:11 +1200

    App layout tweaks

--- src/App.tsx
+++ src/App.tsx
@@ -1,14 +1,27 @@
 import React from 'react';
 import './App.css';
+import { ThemeProvider, createMuiTheme } from '@material-ui/core/styles';
+import { purple } from '@material-ui/core/colors';
+import Container from '@material-ui/core/Container';
 import RegisterCardForm from './components/CreditCardForm';
 import Header from './components/Header';
 
+const theme = createMuiTheme({
+	palette: {
+		primary: {
+			main: purple[300],
+		},
+	},
+});
+
 const App: React.FC = () => {
 	return (
-		<>
+		<ThemeProvider theme={theme}>
 			<Header />
-			<RegisterCardForm />
-		</>
+			<Container maxWidth="lg">
+				<RegisterCardForm />
+			</Container>
+		</ThemeProvider>
 	);
 };
 
