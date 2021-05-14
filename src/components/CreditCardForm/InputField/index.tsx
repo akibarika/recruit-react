7b4f8ade5e70be7a9a68686b26046a9734f491f8
Akibarika <lxclxc89816@gmail.com>
2021-05-14 12:37:41 +1200

    Clean up InputField

--- src/components/CreditCardForm/InputField/index.tsx
+++ src/components/CreditCardForm/InputField/index.tsx
@@ -1,19 +1,13 @@
 import React from 'react';
 import { TextField } from '@material-ui/core';
-import { createStyles, makeStyles, Theme } from '@material-ui/core/styles';
 
 type Props = React.ComponentProps<typeof TextField> & {
 	value?: string | null;
 	errorText?: string | null;
 };
-const useStyles = makeStyles((theme: Theme) =>
-	createStyles({
-		input: {},
-	})
-);
+
 const InputField: React.FC<Props> = (props: Props) => {
 	const { errorText, value, ...restOfProps } = props;
-	const classes = useStyles();
 	return (
 		<TextField
 			variant="outlined"
@@ -22,9 +16,6 @@ const InputField: React.FC<Props> = (props: Props) => {
 			required
 			fullWidth
 			InputLabelProps={{ shrink: true }}
-			InputProps={{
-				className: classes.input,
-			}}
 			value={value}
 			{...restOfProps}
 		/>
