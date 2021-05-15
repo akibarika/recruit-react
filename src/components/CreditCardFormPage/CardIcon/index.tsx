e2aa93c3e2f1b5f1d9ee1098c556c7699fe2d407
Akibarika <lxclxc89816@gmail.com>
2021-05-15 16:44:19 +1200

    Experiment: show credit card icon - create cardIcon

---
+++ src/components/CreditCardFormPage/CardIcon/index.tsx
@@ -0,0 +1,36 @@
+import React from 'react';
+import cardValidator from 'card-validator';
+import { Icon } from 'rsuite';
+import { IconNames } from 'rsuite/lib/Icon/Icon';
+
+type CardIconProps = {
+	cardNumber: string;
+};
+
+const CardIcon: React.FC<CardIconProps> = (props: CardIconProps) => {
+	const { cardNumber } = props;
+	const { card } = cardValidator.number(cardNumber);
+
+	let className: IconNames;
+	switch (card?.type) {
+		case 'visa':
+			className = `cc-visa`;
+			break;
+		case 'mastercard':
+			className = `cc-mastercard`;
+			break;
+		case 'discover':
+			className = `cc-discover`;
+			break;
+		case 'american-express':
+			className = `cc-amex`;
+			break;
+		default:
+			className = `credit-card`;
+			break;
+	}
+
+	return <Icon icon={className} size="lg" />;
+};
+
+export default CardIcon;
